aa147bb14c88201ab65594b246127ae2e58e6f0d
Alumnos <Alumnos@A303-14.upc.edu.pe>
2015-03-28 12:06:58 -0500

    gg2

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -15,7 +15,7 @@ public class PruebaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int gg = 0;
     }
     
 }
